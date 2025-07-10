3a200a3fa73aea03af4c58cfd218d47fb380b632
Jose Garcia <josephtg107@gmail.com>
2025-07-09 23:49:11 -0500

    [Build] Se comenzo el UI para la app de la Dra-Alejandra-App

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,28 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { Tabs } from 'expo-router';
+import './globals.css';
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: '#3b82f6', // blue-500
+        tabBarLabelStyle: { fontSize: 14, fontWeight: 'bold' },
+        tabBarStyle: { backgroundColor: '#fff' },
+      }}
+    >
+      <Tabs.Screen
+        name="inicio"
+        options={{ title: 'Inicio' }}
+      />
+      <Tabs.Screen
+        name="ejercicios"
+        options={{ title: 'Ejercicios' }}
+      />
+      <Tabs.Screen
+        name="perfil"
+        options={{ title: 'Perfil' }}
+      />
+    </Tabs>
   );
 }
---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">Este es el inicio</Text>
+    </View>
+  );
+}
