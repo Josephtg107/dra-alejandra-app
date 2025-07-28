c17b56c2438a4e3337e16efcde32be7617c3c314
Jose <jose@terameg.com>
2025-07-28 17:03:22 -0500

    [Build] Added some auth pages

---
+++ lib/appwrite.ts
@@ -0,0 +1,7 @@
+export const appwriteConfig = {
+    endpoint: process.env.EXPO_APPWRITE_ENDPOINT,
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    platform: "com.zensolutions.DraAlejandraApp",
+    databaseId: '686f48a60036b1bb48e2',
+    userCollectionId: '6887ee970004551696f6',
+}
